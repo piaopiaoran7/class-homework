34eaa67467679abd38ac12565f1988273ab3d4af
piaopiaoran7 <3086363057@qq.com>
2026-06-04 19:30:46 +0800

    v2:升级程序，新增scanf键盘输入与内容回显功能

--- ex01/hello.c
+++ ex01/hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main(void)
 {
-    printf("Hello World!\n");
+    char buf[128];
+    printf("请输入一段文字：");
+    scanf("%s", buf);
+    printf("你输入的内容：%s\n", buf);
     return 0;
 }
